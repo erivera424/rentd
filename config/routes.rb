7b13fb93b83522f9e27692dd3eb1654dc36be5df
Shnai Johnson <shnai.johnson@gmail.com>
2023-06-02 12:58:04 +0800

    new changes to booking

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index] do
+    patch "approve"
+    patch "decline"
+  end
 end
